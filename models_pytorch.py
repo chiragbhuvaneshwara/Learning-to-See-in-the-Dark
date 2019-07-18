0683678d083290513208a814dbc3e979b1200ee8
“chiragbhuvaneshwara” <chiragbhuvaneshwar@gmail.com>
2019-07-18 22:59:26 +0200

    added batch norm and instance norm models

--- models_pytorch.py
+++ models_pytorch.py
@@ -128,6 +128,205 @@ class unet(nn.Module):
 
         return x
 ##################################################################################################
+
+##################################################################################################
+## Unet Batch norm model
+class double_conv_bn(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.LeakyReLU(inplace=True)
+        )
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class down_bn(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv_bn(in_ch, out_ch)
+        )
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+class up_bn(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
+
+        self.conv = double_conv_bn(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
+                        diffY // 2, diffY - diffY//2))
+        
+        # for padding issues, see 
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class unet_bn(nn.Module):
+
+    def __init__(self):
+        super(unet,self).__init__()
+        
+        # https://github.com/alishdipani/U-net-Pytorch/blob/master/train_Unet.py
+        self.inc = double_conv_bn(3, 64)
+        self.down1 = down_bn(64, 128)
+        self.down2 = down_bn(128, 256)
+        self.down3 = down_bn(256, 512)
+        self.down4 = down_bn(512, 512)
+        self.up1 = up_bn(1024, 256)
+        self.up2 = up_bn(512, 128)
+        self.up3 = up_bn(256, 64)
+        self.up4 = up_bn(128, 64)
+        self.out1 = nn.Conv2d(64, 3, 3, padding=1)
+        #self.out2 = DepthToSpace(2)
+
+    def forward(self,x):
+        
+        #print('#################################')
+        #print(x.size())
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.out1(x)
+        #x = self.out2(x)
+        #print(x.size())
+
+        return x
+##################################################################################################
+
+##################################################################################################
+## Unet Instance norm model
+class double_conv_in(nn.Module):
+
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.InstanceNorm2d(out_ch),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.InstanceNorm2d(out_ch),
+            nn.LeakyReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class down_in(nn.Module):
+
+    def __init__(self, in_ch, out_ch):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv_in(in_ch, out_ch)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+class up_in(nn.Module):
+
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
+
+        self.conv = double_conv_in(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
+                        diffY // 2, diffY - diffY//2))
+        
+        # for padding issues, see 
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class unet_in(nn.Module):
+
+    def __init__(self):
+        super(unet,self).__init__()
+        
+        # https://github.com/alishdipani/U-net-Pytorch/blob/master/train_Unet.py
+        self.inc = double_conv_in(3, 64)
+        self.down1 = down_in(64, 128)
+        self.down2 = down_in(128, 256)
+        self.down3 = down_in(256, 512)
+        self.down4 = down_in(512, 512)
+        self.up1 = up_in(1024, 256)
+        self.up2 = up_in(512, 128)
+        self.up3 = up_in(256, 64)
+        self.up4 = up_in(128, 64)
+        self.out1 = nn.Conv2d(64, 3, 3, padding=1)
+        #self.out2 = DepthToSpace(2)
+
+    def forward(self,x):
+        
+        #print('#################################')
+        #print(x.size())
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.out1(x)
+        #x = self.out2(x)
+        #print(x.size())
+
+        return x
+##################################################################################################
 class simpleUNET(nn.Module):
 
     def __init__(self):
