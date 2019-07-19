07d08c06dcc9cfd8bef46ef14be5590f61d35530
“chiragbhuvaneshwara” <chiragbhuvaneshwar@gmail.com>
2019-07-19 03:58:19 +0200

    added GAN model

--- gan_models.py
+++ gan_models.py
@@ -1,9 +1,17 @@
+import os
+import torch
+import torch.nn as nn
+from skimage import io
+import numpy as np
+from torch.autograd import Variable
+import torch.nn.functional as F
+
 ##################################################################################################
 ## Unet Instance norm model
 class double_conv_in(nn.Module):
 
     def __init__(self, in_ch, out_ch):
-        super(double_conv, self).__init__()
+        super(double_conv_in, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.InstanceNorm2d(out_ch),
@@ -21,7 +29,7 @@ class double_conv_in(nn.Module):
 class down_in(nn.Module):
 
     def __init__(self, in_ch, out_ch):
-        super(down, self).__init__()
+        super(down_in, self).__init__()
         self.mpconv = nn.Sequential(
             nn.MaxPool2d(2),
             double_conv_in(in_ch, out_ch)
@@ -34,7 +42,7 @@ class down_in(nn.Module):
 class up_in(nn.Module):
 
     def __init__(self, in_ch, out_ch, bilinear=True):
-        super(up, self).__init__()
+        super(up_in, self).__init__()
 
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
@@ -65,8 +73,10 @@ class up_in(nn.Module):
 class unet_in_generator(nn.Module):
 
     def __init__(self):
-        super(unet_in_generator,self).__init__()
+        super(unet_in_generator,self).__init__(train)
         
+        self.train = train
+
         # https://github.com/alishdipani/U-net-Pytorch/blob/master/train_Unet.py
         self.inc = double_conv_in(3, 64)
         self.down1 = down_in(64, 128)
@@ -80,7 +90,7 @@ class unet_in_generator(nn.Module):
         self.out1 = nn.Conv2d(64, 3, 3, padding=1)
         #self.out2 = DepthToSpace(2)
 
-    def forward(self,x):
+    def forward(self,x, z):
         
         #print('#################################')
         #print(x.size())
@@ -89,6 +99,11 @@ class unet_in_generator(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
+
+        x5 += z
+        # if self.train == True:
+        #     x5 += torch.rand(x5.size())
+
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
@@ -99,3 +114,29 @@ class unet_in_generator(nn.Module):
 
         return x
 ##################################################################################################
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, 3, 1, 1), nn.MaxPool2d(2,2), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
+        self.model = nn.Sequential(
+            *discriminator_block(3, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128)
+        )
+
+        self.adv_layer = nn.Sequential(nn.Linear(30*45 , 1), nn.Sigmoid())
+
+    def forward(self, img):
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+
+        return validity
--- gan_train_Sony_pytorch.py
+++ gan_train_Sony_pytorch.py
@@ -11,7 +11,7 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 from skimage.measure import compare_ssim
 
-from models_pytorch import *
+from gan_models import *
 from datasetLoader_pytorch import SeeingIntTheDarkDataset
 
 trans = transforms.ToPILImage()
@@ -88,45 +88,24 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 
 def trainAndTestModel(name):
 
-    # Initialize the model for this run
-    if name == 'simpleUNET':
-        model = simpleUNET()
-        # selectedModel = simpleUNET()
+    generator = unet_in_generator(train=True)
+    discriminator = Discriminator()
 
-    elif name == 'unet':
-        model = unet()
-        # selectedModel = unet()
+    generator.to(device)
+    discriminator.to(device)
 
-    elif name == 'FPN':
-        model = FPN(Bottleneck, [2,2,2,2])
-
-    elif name == 'unet_bn':
-        model = unet_bn()
-
-    elif name == 'unet_in':
-        model = unet_in()
-    
-    elif name == 'unet_d':
-        model = unet_d()
-
-    else:
-        print('Name variable passed is incorrect')
-        return None
-
-    model.apply(weights_init)
-
-    # Print the model we just instantiated
-    print(model)
-
-    model.to(device)
+    generator.apply(weights_init)
+    discriminator.apply(weights_init)
 
     # Loss and optimizer
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=reg)
+    criterion = nn.BCELoss()
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=learning_rate, weight_decay=reg)
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=learning_rate, weight_decay=reg)
 
     # Train the model
     lr = learning_rate
-    Loss = []                           #to save all the model losses
+    GLoss = []                           #to save all the model losses
+    DLoss = []
     valMSE = []
     valSSIM = []
     total_step = len(train_loader)
@@ -137,23 +116,47 @@ def trainAndTestModel(name):
             in_images = in_images.type(torch.FloatTensor).to(device)
             exp_images = exp_images.type(torch.FloatTensor).to(device)
 
-            # Forward pass
-            outputs = model(in_images)
+            # Adversarial ground truths
+            valid = torch.ones([batch_size, 1]).to(device) # Discriminator Label to real
+            fake = torch.zeros([batch_size, 1]).to(device) # Discriminator Lab
+            # valid = Variable(torch.Tensor(1).fill_(1.0), requires_grad=False)
+            # fake = Variable(torch.Tensor(1).fill_(0.0), requires_grad=False)
 
-            loss = criterion(outputs, exp_images)
-            Loss.append(loss)               #save the loss so we can get accuracies later
+            ###############################
+            # Generator update
+            # Forward pass
+            z = torch.rand((batch_size, 30, 45, 512))
+
+            gen_images = generator(in_images, z)
+            g_loss = criterion(discriminator(gen_images), valid)
+            GLoss.append(g_loss)               #save the loss so we can get accuracies later
 
             # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            optimizer_G.zero_grad()
+            g_loss.backward()
+            optimizer_G.step()
+
+            ###############################
+            # Discriminator update
+            # Measure discriminator's ability to classify real from generated samples
+            real_loss = criterion(discriminator(exp_images), valid)
+            fake_loss = criterion(discriminator(gen_images.detach()), fake)
+            
+            d_loss = (real_loss + fake_loss) / 2
+            DLoss.append(d_loss)
+
+            optimizer_D.zero_grad()
+            d_loss.backward()
+            optimizer_D.step()
+
             if (i+1) % 20 == 0:
-                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+                print ('Epoch [{}/{}], Step [{}/{}], Gen_Loss: {:.6f}, Disc_Loss: {:.6f}'
+                    .format(epoch+1, num_epochs, i+1, total_step, g_loss.item(), d_loss.item() ) )
         
         # Code to update the lr
         lr *= learning_rate_decay
-        update_lr(optimizer, lr)
+        update_lr(optimizer_D, lr)
+        update_lr(optimizer_G, lr)
 
         with torch.no_grad():
 
@@ -162,7 +165,9 @@ def trainAndTestModel(name):
             for in_images, exp_images in val_loader:
                 in_images = in_images.to(device)
                 exp_images = exp_images.to(device)
-                outputs = model(in_images)
+                z = torch.zeros((batch_size, 30, 45, 512))
+                outputs = generator(in_images, z)
+
                 MSE += torch.sum((outputs - exp_images) ** 2)
                 #outputs = outputs.cpu()
                 outputs_np = outputs.permute(0, 2, 3, 1).cpu().numpy() 
@@ -181,7 +186,7 @@ def trainAndTestModel(name):
             current_MSE = MSE/total
             valMSE.append(current_MSE)
             if current_MSE <= np.amin(valMSE):
-                torch.save(model.state_dict(),'models/ESmodel'+str(epoch+1)+'.ckpt')
+                torch.save(generator.state_dict(),'models/ESmodel'+str(epoch+1)+'.ckpt')
 
             print('Avg Validation MSE on all the {} Val images is: {} '.format(total, current_MSE))
             print('Avg Validation SSIM on all the {} Val images is: {} '.format(total, overallSSIM/total))
@@ -203,9 +208,17 @@ def trainAndTestModel(name):
     plt.savefig('plots/'+name+title+'.png')
     plt.close()
 
-    plt.plot(Loss)
-    title='Loss_vs_Iterations'
-    plt.ylabel('Loss')
+    plt.plot(GLoss)
+    title='GeneratorLoss_vs_Iterations'
+    plt.ylabel('Generator Loss')
+    plt.xlabel('Iterations')
+    plt.title(title)
+    plt.savefig('plots/'+name+title+'.png')
+    plt.close()
+
+    plt.plot(DLoss)
+    title='DiscriminatorLoss_vs_Iterations'
+    plt.ylabel('Discriminator Loss')
     plt.xlabel('Iterations')
     plt.title(title)
     plt.savefig('plots/'+name+title+'.png')
@@ -215,7 +228,7 @@ def trainAndTestModel(name):
 
     # last_model = model
     best_id = np.argmin(valMSE)
-    bestESmodel = model
+    bestESmodel = generator
 
     bestESmodel.load_state_dict(torch.load('models/ESmodel'+str(best_id+1)+'.ckpt'))
     bestESmodel = bestESmodel.to(device)
@@ -281,12 +294,8 @@ def trainAndTestModel(name):
 
 ###############################################################################################################################################
 # parameters to select different models ==> Just change here. 
-# name = 'simpleUNET'
 name = 'unet'
-# name = 'unet_bn'
-# name = 'unet_in'
-# name = 'unet_d'
-# name = 'FPN'
+
 trainAndTestModel(name)
 
 
