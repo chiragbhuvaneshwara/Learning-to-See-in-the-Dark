e91ce4c7876f37fa3fd5bb6c965e756c5cc188ff
“chiragbhuvaneshwara” <chiragbhuvaneshwar@gmail.com>
2019-07-22 20:27:27 +0200

    minor

--- gan_train_Sony_pytorch.py
+++ gan_train_Sony_pytorch.py
@@ -230,7 +230,7 @@ class unet_in_generator(nn.Module):
         self.up4 = up_in(128, 64)
         self.out1 = nn.Conv2d(64, 3, 3, padding=1)
         #self.out2 = DepthToSpace(2)
-	    self.sigmoid = nn.Sigmoid()	
+        self.sigmoid = nn.Sigmoid()	
 
     def forward(self,x):
         
@@ -253,7 +253,7 @@ class unet_in_generator(nn.Module):
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         x = self.out1(x)
-	    x = self.sigmoid(x)
+        x = self.sigmoid(x)
         #x = self.out2(x)
         #print(x.size())	
 
